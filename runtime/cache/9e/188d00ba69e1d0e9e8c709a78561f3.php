dedc53bcdd7be6f5ba019abb77983e3539248da6
My Xcode <wu775825@163.com>
2018-04-24 16:47:43 +0800

    fastAdmin version 1.0

---
+++ runtime/cache/9e/188d00ba69e1d0e9e8c709a78561f3.php
@@ -0,0 +1,4 @@
+<?php
+//000000000000
+ exit();?>
+a:1:{i:0;a:11:{s:2:"id";i:1;s:10:"oldversion";s:7:"1.1.1,2";s:10:"newversion";s:5:"1.2.1";s:11:"packagesize";s:3:"20M";s:7:"content";s:12:"更新内容";s:11:"downloadurl";s:39:"https://www.fastadmin.net/download.html";s:7:"enforce";i:1;s:10:"createtime";i:1520425318;s:10:"updatetime";i:0;s:5:"weigh";i:0;s:6:"status";s:6:"normal";}}
